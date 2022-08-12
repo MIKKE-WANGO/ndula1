95e2fcab0c0c5e37a59f3433f8d20926e97af201
MIKKE-WANGO <mikemundati@gmail.com>
2022-08-12 13:37:13 +0300

    heroku db

--- ndula/settings.py
+++ ndula/settings.py
@@ -107,6 +107,12 @@ DATABASES = {
 }
 
 
+#use db provided by heroku
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+
 #setting up email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
